34e8bd2b8d8e7a8079e73c0cd3036f8fe481774f
Artur Vedmedenko <vedmedenko.a@gmail.com>
2024-08-03 11:58:25 +0300

    add homework-8

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById('name-input');
-const output = document.getElementById('name-output');
-
-input.addEventListener('input', () => {
-  output.textContent = input.value.trim() || 'Anonymous';
-});
